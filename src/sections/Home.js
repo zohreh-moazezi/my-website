616e1ad4ed5381b63f33133ed904ff53abf48c0c
zohreh moazezi <zohre.m548@gmail.com>
2023-11-26 17:24:14 +0330

    changes2

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="texts" >
           <h3 className="name">Hi I'm</h3>
           <h1 className="name">Zohreh Moazezi</h1>
-          <h5>Frontend Developer salam</h5>
+          <h5>Frontend Developer</h5>
         </div>
       </div>
     </>
